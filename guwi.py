972b2662ef6d99682e520dd580d947e16048a894
ocsigsampit <oc.sig.sampit@gmail.com>
2019-03-24 21:53:37 +0700

    pindah ke python 3

--- guwi.py
+++ guwi.py
@@ -1,4 +1,6 @@
-from Tkinter import *
+from tkinter import *
+from tkinter import messagebox
+import sys
 
 tampilan = Tk()
 # judul = Label(tampilan,"Pasangan Calon Pemilihan Presiden 2019")
@@ -9,10 +11,14 @@ tombol1.pack(side=LEFT, padx=10)
 tombol2.pack(side=RIGHT, padx=10)
 
 def tampil01():
-    print "Pasangan No Urut 01. Jokowi - Ma'ruf Amin"
+    print("Pasangan No Urut 01. Jokowi - Ma'ruf Amin")
+    messagebox.showinfo("01","Pasangan No Urut 01. Jokowi - Ma'ruf Amin")
+    #sys.exit(0)
 	
 def tampil02():
-    print "Pasangan No Urut 02. Prabowo - Sandi"
+    print("Pasangan No Urut 02. Prabowo - Sandi")
+    messagebox.showinfo("02","Pasangan No Urut 02. Prabowo - Sandiaga Uno")
+    #sys.exit(0)
 	
 tombol1.configure(command=tampil01)
 tombol2.configure(command=tampil02)

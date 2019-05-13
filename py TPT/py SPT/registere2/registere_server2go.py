09a5733d2f50a23d7625f75c6420bec3329778ef
ocsigsampit <oc.sig.sampit@gmail.com>
2019-05-13 22:03:00 +0700

    berhasil membaca semua halaman file pdf

--- py TPT/py SPT/registere2/registere_server2go.py
+++ py TPT/py SPT/registere2/registere_server2go.py
@@ -4,17 +4,31 @@ import sys
 import pymysql
 import mysql.connector
 import os
-from time import sleep
 
 db = pymysql.connect(db="pengemasan", user="root", passwd="", host="127.0.0.1", port=3306)
-
 cursor = db.cursor()
 
 cursor.execute("TRUNCATE TABLE tb_register_spt")
 db.commit()
 
-berkasRegister = []
+def cariNomorRegister(teks):
+	noregs  = re.findall(r'REG\-\d+\/\w+\/\w+\.\d+\/\w+\.\d+\/\d{4}',teks)
+	noreg  = noregs[0]
+	noreg  = str(noreg)
+	
+	return noreg
+	
+def cariTanggalRegister(teks):
+	tgl_register = teks.split("SPT")
+	tgl_register = tgl_register[0]
+	tgl_register = tgl_register.split(" : ")
+	tgl_register = tgl_register[1]
+	tgl_register = tgl_register.strip()	
+	
+	return tgl_register
 
+berkasRegister = []
+jumlahBerkasRegister = 0
 jumlahBPS = 0
 
 for filename in os.listdir('.'):
@@ -26,42 +40,57 @@ berkasRegister.sort(key=str.lower)
 for berkas in berkasRegister:
 	bukaBerkas = open(berkas,'rb')
 	bacaBerkas = PyPDF2.PdfFileReader(bukaBerkas)
-	jumlahHalaman = bacaBerkas.getNumPages()
+	bacaHalamanSatu = bacaBerkas.getPage(0)
+	hasilBacaHalamanSatu = bacaHalamanSatu.extractText()
 	
+	NomorRegister = cariNomorRegister(hasilBacaHalamanSatu)
+	TanggalRegister = cariTanggalRegister(hasilBacaHalamanSatu)
+	
+	print("===============================================================================================================")
+	print("No Register : " + NomorRegister + " Tanggal Register : " + TanggalRegister)
+	print("===============================================================================================================")
+	print("\n")	
+	
+	jumlahHalaman = bacaBerkas.getNumPages()
+	jumlahBerkasRegister += 1
+		
 	for halaman in range(jumlahHalaman):
 		bacaHalaman = bacaBerkas.getPage(halaman)
 		hasilBaca = bacaHalaman.extractText()
 		
-		if halaman == 0:
-			noregs  = re.findall(r'REG\-\d+\/\w+\/\w+\.\d+\/\w+\.\d+\/\d{4}',hasilBaca)
-			noregs  = noregs[0]
-			noregs  = str(noregs)
-
-			tgl_register = hasilBaca.split("SPT")
-			tgl_register = tgl_register[0]
-			tgl_register = tgl_register.split(" : ")
-			tgl_register = tgl_register[1]
-			tgl_register = tgl_register.strip()
-
+		print("Halaman ",str(halaman + 1))
+		
 		npwps = re.findall(r'\d{2}\.\d{3}\.\d{3}\.\d{1}\-\d{3}\.\d{3}',hasilBaca)
 		bpss  = re.findall(r'S\-\d+\/\w+\/\w+\.\d+\/\w+\.\d+\/\d{4}',hasilBaca)
 	
 		kamus = dict(zip(bpss, npwps))
 		
-	for x,y in kamus.items():
-		cursor.execute("INSERT INTO tb_register_spt (no_reg,tgl_str_reg,no_bps,npwp) VALUES ('" + noregs + "','" + tgl_register + "','" + x + "','" + y + "')")
-		db.commit()
-		#print("Halaman : ",halaman,"\n")
-		barisPertama = "Nomor Register : " + str(noregs) + " Tgl Register : " + str(tgl_register) + " No BPS : " + str(x) + " NPWP : " + (y) + "\n"
-		strBarisPertama = str(barisPertama)
-		strBarisPertama = strBarisPertama + "\n"
-		#print("Str barisPertama : " + strBarisPertama)
-		sys.stdout.write(strBarisPertama)
-		
-		jumlahBPS = jumlahBPS + 1
-		strJumlahBPS = str(jumlahBPS)
-		strJumlahBPS = "Jumlah BPS : " + strJumlahBPS + "\n"
-		#print(strJumlahBPS)
-		sys.stdout.write(strJumlahBPS)
-		
-		sys.stdout.flush()
+		for x,y in kamus.items():
+			barisPertama = "Nomor Register : " + str(NomorRegister) + " Tgl Register : " + str(TanggalRegister) + " No BPS : " + str(x) + " NPWP : " +  str(y)
+			print("------------------------------------------------------------------------------------")
+			print(barisPertama)
+			print("------------------------------------------------------------------------------------")
+			cursor.execute("INSERT INTO tb_register_spt (no_reg,tgl_str_reg,no_bps,npwp) VALUES ('" + NomorRegister + "','" + TanggalRegister+ "','" + x + "','" + y + "')")
+			db.commit()
+			jumlahBPS += 1
+			print("Jumlah BPS : " + str(jumlahBPS))
+			print("------------------------------------------------------------------------------------")
+print("\n")		
+print("Jumlah Berkas Register : ",jumlahBerkasRegister,"Register")
+
+cursor.execute("UPDATE tb_register_spt SET tgl_reg = SUBSTRING_INDEX(tgl_str_reg,' ',1)")
+db.commit()
+
+cursor.execute("UPDATE tb_register_spt SET bln_reg = SUBSTRING_INDEX(SUBSTRING_INDEX(tgl_str_reg,' ',2),' ',-1)")
+db.commit()
+
+cursor.execute("UPDATE tb_register_spt SET thn_reg = SUBSTR(tgl_str_reg,-4)")
+db.commit()
+
+cursor.execute("UPDATE tb_register_spt SET kd_jns_spt = SUBSTRING_INDEX(SUBSTRING_INDEX(no_bps,'/',-4),'/',1)")
+db.commit()
+
+db.close()
+
+print("\n")
+print("================== THE JOB IS DONE ! =======================\n")
